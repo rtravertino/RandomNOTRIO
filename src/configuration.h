c76436088c36957cfada67bb18e9d215693f41a6
rtravertino <174337562+rtravertino@users.noreply.github.com>
2024-09-19 10:03:39 +0200

    reduce L1 scratchpad

--- src/configuration.h
+++ src/configuration.h
@@ -68,7 +68,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_SCRATCHPAD_L2      262144
 
 //Scratchpad L1 size in bytes. Must be a power of two (minimum 64) and less than or equal to RANDOMX_SCRATCHPAD_L2.
-#define RANDOMX_SCRATCHPAD_L1      32768
+#define RANDOMX_SCRATCHPAD_L1      16384
 
 //Jump condition mask size in bits.
 #define RANDOMX_JUMP_BITS          8
